1bc6c3bfd0403fc26617af16d497e3223cc94290
Rogério Fontes <43037669+rogeriofontes05@users.noreply.github.com>
2022-11-28 21:54:02 -0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,94 @@
+let questions = [
+    {
+        question: 'Quantas pragas foram enviadas ao Egito?',
+        options: [
+            '7',
+            '10',
+            '3',
+            '12'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Quem foi lançado na cova dos leões?',
+        options: [
+            'Paulo',
+            'José',
+            'Daniel'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual instrumento Davi gostava de tocar?',
+        options: [
+            'Tambor',
+            'Harpa',
+            'Flauta',
+            'Corneta'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Quando Jesus nasceu, onde Ele foi colocado?',
+        options: [
+            'Foi colocado numa cama.',
+            'Foi colocado numa manjedoura.',
+            'Foi colocado em um trono.'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Quando a mulher com fluxo de sangue tocou nas vestes de Jesus, no que ela pensava?',
+        options: [
+            '"Se eu tão somente tocar em seu manto, ficarei curada".',
+            '"Tenho que chamar a atenção de Jesus para ser curada".',
+            '"Tenho que interromper o caminho de Jesus".'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Na parábola do semeador, quais sementes que cresceram e deram uma boa colheita?',
+        options: [
+            'As sementes que caíram nas pedras.',
+            'As sementes que caíram em boa terra.',
+            'As sementes que caíram entre os espinhos.'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Jesus comparou o Reino de Deus a qual semente?',
+        options: [
+            'Ao grão de mostarda.',
+            'Ao grão de milho.',
+            'A semente de uva.'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Quem são conhecidos como os patriarcas na Bíblia?',
+        options: [
+            'Jesus, Maria e José',
+            'Pedro, João e Tiago',
+            'Abraão, Isaque e Jacó'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Quem ensinou os discípulos a oração do "Pai nosso"?',
+        options: [
+            'Os fariseus',
+            'Jesus',
+            'Paulo'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Quem foi o "assistente" do profeta Elias?',
+        options: [
+            'Eliseu',
+            'Enoque',
+            'Jonas',
+        ],
+        answer: 0
+    }
+];
